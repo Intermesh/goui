05f764b49f5479d0c5fbe9e9d95e354c65075ec6
Merijn Schering <mschering@intermesh.nl>
2022-07-14 17:35:53 +0200

    Build

--- examples/Index.ts
+++ examples/Index.ts
@@ -1,12 +1,4 @@
-import {PlayGround} from "./PlayGround.js";
-import {client} from "../script/api/Client.js";
-import {cards} from "../script/component/CardContainer.js";
-import {root} from "../script/component/Root.js";
-import {router} from "../script/Router.js";
-import {comp, Component} from "../script/component/Component.js";
-import {btn} from "../script/component/Button.js";
-import {Translate} from "../script/Translate.js";
-import {PluginManager} from "../build/component/PluginManager.js";
+import {btn, cards, client, comp, Component, PluginManager, root, router, Translate} from "../script/goui.js";
 import {PlaygroundTable} from "./PlaygroundTable.js";
 
 // Setup Group-Office connection
@@ -19,11 +11,11 @@ client.uri = "http://host.docker.internal:6780/api/";
  * @param id
  * @param mod
  */
-function loadCard(cls: string, id: string, mod = `./${cls}.js` ) : Promise<Component>{
+function loadCard(cls: string, id: string, mod = `./${cls}.js`): Promise<Component> {
 	let index = main.findItemIndex(id);
 	if (index == -1) {
 
-		return import(mod).then((mods:any) => {
+		return import(mod).then((mods: any) => {
 			mods[cls].setId(id);
 			index = main.items.add(mods[cls]);
 			main.activeItem = index;
@@ -46,9 +38,9 @@ router.on("change", () => {
 	console.warn(`Missing translations`, Translate.missing);
 });
 
-const loadPlayground = async ()  => {
+const loadPlayground = async () => {
 	const mods = await import("./PlayGround.js");
-	if(!main.items.has(mods.PlayGround)) {
+	if (!main.items.has(mods.PlayGround)) {
 		main.items.add(mods.PlayGround);
 	}
 	mods.PlayGround.show();
@@ -57,7 +49,7 @@ const loadPlayground = async ()  => {
 }
 
 //register a plugin for Playground table
-PluginManager.register("PlaygroundTable", function(this:PlaygroundTable) {
+PluginManager.register("PlaygroundTable", function (this: PlaygroundTable) {
 	this.el.classList.add("cls-added-by-plugin");
 });
 
@@ -73,17 +65,16 @@ router
 
 	.add(() => {
 
-			let index = main.findItemIndex("notfound");
-			if(index == -1) {
-				main.items
-					.add(
-
-						comp({
+		let index = main.findItemIndex("notfound");
+		if (index == -1) {
+			main.items
+				.add(
+					comp({
 							cls: "pad",
 							id: "notfound"
 						},
-							comp({
-								html: `
+						comp({
+							html: `
 									<h1>Heading 1</h1>
 									<h2>Heading 2</h2>
 									<h3>Heading 3</h3>
@@ -94,33 +85,30 @@ router
 									
 									<p><a href="#playground">Visit play ground</a></p>
 								`,
-							}),
+						}),
 
-							comp({cls: "pad"},
-								comp({tagName: "h1", text: "Heading 1"}),
-								comp({tagName: "h2", text: "Heading 2"})
-							),
-
-							comp({cls: "pad"},
-								comp({tagName: "h3", text: "Heading 3"}),
-								comp({tagName: "h4", text: "Heading 4"})
-							),
-
-							btn({
-								text: "Text"
-							})
-
+						comp({cls: "pad"},
+							comp({tagName: "h1", text: "Heading 1"}),
+							comp({tagName: "h2", text: "Heading 2"})
 						),
 
+						comp({cls: "pad"},
+							comp({tagName: "h3", text: "Heading 3"}),
+							comp({tagName: "h4", text: "Heading 4"})
+						),
 
-					);
+						btn({
+							text: "Text"
+						})
+					),
+				);
 
-				index = main.items.count() - 1;
-			}
-			main.activeItem = index;
-		})
+			index = main.items.count() - 1;
+		}
+		main.activeItem = index;
+	})
 
-		.start();
+	.start();
 
 
 // });
