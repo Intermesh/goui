0efbe8d943f3bcc87a1e8afa1060430c91d49145
Merijn Schering <mschering@intermesh.nl>
2023-06-20 15:01:25 +0200

    AutocompleteField.ts fixed

--- script/component/form/AutocompleteField.ts
+++ script/component/form/AutocompleteField.ts
@@ -125,6 +125,14 @@ export class AutocompleteField<T extends List = List> extends TextField {
 		})
 	}
 
+	get value(): any {
+		return this._value;
+	}
+
+	set value(v: any) {
+		super.value = v;
+	}
+
 
 	protected internalRender(): HTMLElement {
 
