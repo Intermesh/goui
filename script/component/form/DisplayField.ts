e71c749d48eca6673877a1f2af4c5dbe89f54f8c
Merijn Schering <mschering@intermesh.nl>
2023-10-16 17:11:49 +0200

    displayfield

--- script/component/form/DisplayField.ts
+++ script/component/form/DisplayField.ts
@@ -9,10 +9,24 @@ import {Component, createComponent} from "../Component.js";
 import {Config} from "../Observable";
 import {Format} from "../../util";
 
+/**
+ * Display field
+ *
+ * Component used to display data only.
+ *
+ * A {@see Form} can be used to edit data but also to present data using display fields
+ */
 export class DisplayField extends Field {
 	private control?: HTMLDivElement;
 
 	protected baseCls = 'goui-display-field'
+
+	/**
+	 * Renderer function for the value of the field
+	 *
+	 * @param v
+	 * @param field
+	 */
 	public renderer: DisplayFieldRenderer = (v:any, field:DisplayField) => Format.escapeHTML(v) ?? "";
 
 	protected createControl(): HTMLElement | undefined {
@@ -35,6 +49,10 @@ export class DisplayField extends Field {
 }
 type DisplayFieldRenderer = (v:any, field:DisplayField) => string|Promise<string>;
 type DisplayFieldConfig = Config<DisplayField, FieldEventMap<DisplayField>> & {
+
+	/**
+	 * Renderer function for the value of the field
+	 */
 	renderer?: DisplayFieldRenderer;
 }
 
