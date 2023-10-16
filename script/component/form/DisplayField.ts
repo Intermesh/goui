91efd7cab6e0f170dc2e38c089f3de449a5f8903
Merijn Schering <mschering@intermesh.nl>
2023-10-16 17:04:25 +0200

    Display fields

--- script/component/form/DisplayField.ts
+++ script/component/form/DisplayField.ts
@@ -7,12 +7,13 @@
 import {Field, FieldEventMap} from "./Field.js";
 import {Component, createComponent} from "../Component.js";
 import {Config} from "../Observable";
+import {Format} from "../../util";
 
 export class DisplayField extends Field {
 	private control?: HTMLDivElement;
 
 	protected baseCls = 'goui-display-field'
-	public renderer = (v:any, field:DisplayField) => v ?? "";
+	public renderer: DisplayFieldRenderer = (v:any, field:DisplayField) => Format.escapeHTML(v) ?? "";
 
 	protected createControl(): HTMLElement | undefined {
 		this.control = document.createElement("div");
@@ -20,13 +21,21 @@ export class DisplayField extends Field {
 	}
 
 	protected internalSetValue(v?: any) {
-		if(this.control)
-			this.control.innerText = this.renderer(v, this);
+		if(this.control) {
+			const str = this.renderer(v, this);
+			if(str instanceof Promise) {
+				str.then(r => {
+					this.control!.innerHTML = r;
+				});
+			} else {
+				this.control.innerHTML = str;
+			}
+		}
 	}
 }
-
+type DisplayFieldRenderer = (v:any, field:DisplayField) => string|Promise<string>;
 type DisplayFieldConfig = Config<DisplayField, FieldEventMap<DisplayField>> & {
-	renderer?: (v:any, field:DisplayField) => string;
+	renderer?: DisplayFieldRenderer;
 }
 
 export const displayfield = (config: DisplayFieldConfig, ...items: Component[]) => createComponent(new DisplayField(config?.tagName), config, items);
