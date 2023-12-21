74ce43e54ff5763a48a1ebc10bafd2cf690758f2
Merijn Schering <mschering@intermesh.nl>
2023-12-21 13:55:32 +0100

    TimeField

--- script/component/form/TimeField.ts
+++ script/component/form/TimeField.ts
@@ -6,55 +6,100 @@ import {DateTime, E} from "../../util";
 
 export class TimeField extends Field {
 
-
-	private static inputFormat = "h:i:a";
-	private static outputFormat = "H:i";
-
-	public input12hr = true;
+	public input12hr = false;
 
 	private hoursInput?: HTMLInputElement;
 	private minutesInput?: HTMLInputElement;
 
-	protected baseCls = "goui-form-field date no-floating-label";
+	protected baseCls = "goui-form-field time no-floating-label";
 	private AMPMInput?: HTMLSelectElement;
 
 	public getValueAsDateTime() {
-		return DateTime.createFromFormat(this.value, TimeField.outputFormat);
+		return DateTime.createFromFormat(this.value, "H:i");
 	}
 
 	protected createControl(): HTMLElement | undefined {
 		const ctrl = E("div").cls("goui hbox");
 
+		if(this.input12hr) {
+			this.el.classList.add('input12hr');
+		}
+
 		const onBlur = function(this:any) {
+			if(!this.value) {
+				return;
+			}
 			this.value = this.value.padStart(2, "0")
 			return true;
 		}
 
+		const onFocus = function(this:any) {
+			this.setSelectionRange(0, this.value.length);
+		};
+
 		this.hoursInput = document.createElement("input");
 		this.hoursInput.classList.add("text");
-		this.hoursInput.classList.add("hours");
+		this.hoursInput.classList.add("hour");
 		this.hoursInput.type = "text";
 		this.hoursInput.pattern = "[0-9]+";
 		this.hoursInput.maxLength = 2;
 		this.hoursInput.onblur = onBlur;
-		// this.hoursInput.onfocus = onFocus;
+		this.hoursInput.onfocus = onFocus;
 		// this.hoursInput.onmouseup = onMouseUp;
-		this.hoursInput.placeholder = "HH";
+		this.hoursInput.placeholder = "--";
 		this.hoursInput.autocomplete = "off";
+		this.hoursInput.onkeyup = ev => {
+
+			if(!/^[0-9]$/i.test(ev.key)) {
+				return;
+			}
+
+			const max = this.input12hr ? "11" : "23";
+
+			if(this.hoursInput!.value > max) {
+				this.hoursInput!.value = max;
+				this.hoursInput!.setSelectionRange(0,2);
+			} else {
+
+				if (this.hoursInput!.value.length == 2) {
+					this.minutesInput!.focus();
+				}
+			}
+		}
 
 		this.minutesInput = document.createElement("input");
 		this.minutesInput.classList.add("text");
-		this.minutesInput.classList.add("hours");
+		this.minutesInput.classList.add("minute");
 		this.minutesInput.type = "text";
 		this.minutesInput.pattern = "[0-9]+";
 		this.minutesInput.maxLength = 2;
-		this.minutesInput.onblur = onBlur;
-		// this.hoursInput.onfocus = onFocus;
-		// this.hoursInput.onmouseup = onMouseUp;
-		this.minutesInput.placeholder = "MM";
+		const hoursInput = this.hoursInput!;
+		this.minutesInput.onblur = function(this:any) {
+			onBlur.call(this);
+			if(!this.value && hoursInput.value) {
+				this.value = "00";
+			}
+		};
+		this.minutesInput.onfocus = onFocus;
+		this.minutesInput.onkeyup = ev => {
+
+			if(!/^[0-9]$/i.test(ev.key)) {
+				return;
+			}
+
+			if(this.minutesInput!.value > "59") {
+				this.minutesInput!.value = "59";
+			}
+
+			if(this.input12hr && this.minutesInput!.value.length == 2) {
+				this.AMPMInput!.focus();
+			}
+
+		}
+		this.minutesInput.placeholder = "--";
 		this.minutesInput.autocomplete = "off";
 
-		ctrl.append(this.hoursInput, this.minutesInput);
+		ctrl.append(this.hoursInput, ":", this.minutesInput);
 
 		if(this.input12hr) {
 			this.AMPMInput = document.createElement("select");
@@ -64,6 +109,7 @@ export class TimeField extends Field {
 			amOpt.innerHTML = "am";
 			amOpt.selected = true;
 			this.AMPMInput.appendChild(amOpt);
+
 
 			const pmOpt = new Option();
 			pmOpt.value = "pm";
