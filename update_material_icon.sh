bdfefd29f0b05e927c2f134e83da33147477f77a
Merijn Schering <mschering@intermesh.nl>
2026-06-22 14:53:09 +0200

    Update with new icons

--- update_material_icon.sh
+++ update_material_icon.sh
@@ -3,9 +3,8 @@ LIST=`curl -s https://raw.githubusercontent.com/google/material-design-icons/mas
 TS="export type MaterialIcon = \n";
 
 while IFS= read -r line; do
-
-  IFS=', ' read -r -a array <<< "$line"
-  TS="$TS \"${array[0]}\" |\n";
+  word="${line%% *}"
+  TS="$TS \"$word\" |\n";
 done <<< "$LIST"
 
 #remove 4 chars to remove " |\n" of the last line
